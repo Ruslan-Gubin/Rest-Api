add37bf270bcdaf8a83735d55f58112c6effec8f
Ruslan-Gubin <ruslan_gubin@rambler.ru>
2022-07-28 16:36:51 +0300

    finished

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("node:path");
+const {v4} = require('uuid');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'client')));
+let CONTACTS = [
+  { id: 1, name: "Ruslan", value: "+7-921-100-20-30", marked: false },
+  { id: 2, name: "Anna", value: "+7-921-200-40-10", marked: false },
+];
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client','index.html'));
+app.use(express.json());
+
+//GET
+app.get("/api/contacts", (req, res) => {
+    res.status(200).json(CONTACTS);
 });
 
-app.listen(3001, () => console.log('Server has been srarted on port 3001...'));
+//POST
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false};
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
+});
 
+//DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+    res.status(200).json({message: 'Контакт был уданен'});
+});
 
+// PUT
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+    CONTACTS[idx] = req.body;
+    res.json(CONTACTS[idx]);
+});
 
+app.use(express.static(path.resolve(__dirname, "client")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "index.html"));
+});
 
-
-
-
-
-
-
-
-
-
-
-
+app.listen(3001, () => console.log("Server has been srarted on port 3001..."));
--- client/frontend.js
+++ client/frontend.js
@@ -1,32 +1,85 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.0/dist/vue.esm.browser.js';
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.7.0/dist/vue.esm.browser.js";
+
+Vue.component("loader", {
+  template: `
+<div style='display: flex;justify-content: center;align-items: center'>
+<div class="spinner-border" role="status">
+<span class="visually-hidden">Loading...</span>
+</div>
+</div>
+`,
+});
 
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            form: {
-                name: '',
-                value: ''
-            },
-            contacts: [
-                {id: 1, name: 'Ruslan', value: '+7-921-100-20-30', marked: false}
-            ],
-        }
+  el: "#app",
+  data() {
+    return {
+      loading: false,
+      form: {
+        name: "",
+        value: "",
+      },
+      contacts: [],
+    };
+  },
+  computed: {
+    canCreate() {
+      return this.form.value.trim() && this.form.name.trim();
     },
-    methods: {
-        createContact() {
-           const {...contact} = this.form
-           
-           this.contacts.push({...contact, id: Date.now()})
-           
-           this.form.name = this.form.value = '';
-        },
-        markContact(id) {
-            const contact = this.contacts.find(c => c.id === id)
-            contact.marked = true;
-        },
-        removeContact(id) {
+  },
+  methods: {
+    async createContact() {
+      const {...contact} = this.form;
 
-        }
-    }
+      const newContact = await request("/api/contacts", "POST", contact);
+
+      this.contacts.push(newContact);
+
+      this.form.name = this.form.value = "";
+    },
+   async markContact(id) {
+       const contact = this.contacts.find((c) => c.id === id);
+      const apdated = await request(`/api/contacts/${id}`, 'PUT', {
+            ...contact,
+            marked: true,
+        });
+
+
+
+      contact.marked = apdated.marked;
+    },
+    removeContact(id) {
+
+        request(`/api/contacts/${id}`, 'DELETE');
+
+      this.contacts = this.contacts.filter(c => c.id !== id);
+    },
+  },
+  async mounted() {
+    this.loading = true;
+    const data = await request("/api/contacts");
+    this.contacts = data;
+    this.loading = false;
+  },
 });
+
+async function request(url, method = "GET", data = null) {
+  try {
+    const headers = {};
+    let body;
+
+    if (data) {
+      headers["Content-Type"] = "application/json";
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body,
+    });
+    return await response.json();
+  } catch (e) {
+    console.warn("Error", e.message);
+  }
+}
